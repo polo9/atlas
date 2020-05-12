244b44d364904d837800b876d061b54d96c7fbb9
Eric Lee <ericl@squareup.com>
2020-05-12 14:09:25 -0400

    Remove pipeline_context_wrapper as it is no longer used

--- atlas/foundations_internal/src/foundations_internal/pipeline_context_wrapper.py
+++
@@ -1,7 +0,0 @@
-#TODO refactor this out
-class PipelineContextWrapper(object):
-    def __init__(self, pipeline_context):
-        self._pipeline_context = pipeline_context
-
-    def pipeline_context(self):
-        return self._pipeline_context
