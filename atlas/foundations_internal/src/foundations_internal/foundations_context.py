779d5d05cf6855d570d9591bf6677b387d43c85d
Eric Lee <ericl@squareup.com>
2020-05-12 17:47:05 -0400

    Remove public facing FoundationsContext.pipeline_context()
    There's no external usage. Moving towards hiding pipeline as
    implementation detail

--- atlas/foundations_internal/src/foundations_internal/foundations_context.py
+++ atlas/foundations_internal/src/foundations_internal/foundations_context.py
@@ -14,15 +14,6 @@ class FoundationsContext(object):
         self._pipeline = pipeline
         self._job_resources = self._default_job_resources()
 
-    def pipeline_context(self):
-        """The current pipeline context associate with the assigned pipeline
-
-        Returns:
-            PipelineContext -- As above
-        """
-
-        return self._pipeline.pipeline_context()
-
     def __getstate__(self):
         raise ValueError('FoundationsContexts do not support serialization')
 
@@ -39,11 +30,11 @@ class FoundationsContext(object):
 
     @property
     def job_id(self):
-        return self.pipeline_context().file_name
+        return self._pipeline.pipeline_context().file_name
 
     @job_id.setter
     def job_id(self, value):
-        self.pipeline_context().file_name = value
+        self._pipeline.pipeline_context().file_name = value
 
     @property
     def job_resources(self):
@@ -58,13 +49,13 @@ class FoundationsContext(object):
 
     def is_in_running_job(self):
         try:
-            return self.pipeline_context().file_name is not None
+            return self._pipeline.pipeline_context().file_name is not None
         except ValueError:
             return False
 
     @property
     def provenance(self):
-        return self.pipeline_context().provenance
+        return self._pipeline.pipeline_context().provenance
 
     def _default_job_resources(self):
         from foundations_internal.job_resources import JobResources
@@ -72,8 +63,8 @@ class FoundationsContext(object):
 
     @property
     def user_name(self):
-        return self.pipeline_context().provenance.user_name
+        return self._pipeline.pipeline_context().provenance.user_name
 
     @user_name.setter
     def user_name(self, value):
-        self.pipeline_context().provenance.user_name = value
+        self._pipeline.pipeline_context().provenance.user_name = value
